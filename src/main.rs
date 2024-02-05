64dcb3714a991698046c2114c92f93c481e92c1f
Jao <joaovictorocosta@gmail.com>
2024-02-05 04:20:10 -0300

    Capture the user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io; 
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess: ");
+
+    let mut guess = String::new(); // new empty string -> same as calling a static method on the String class
+
+    io::stdin()
+        .read_line(&mut guess) // read_line returns a Result type
+        .expect("Failed to read line"); // expect is a method on Result types
+
+    println!("You guessed: {}", guess);   
 }
