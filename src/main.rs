ea2d4b5b48a444df6e1e7f296aedade8d292b128
Jao <joaovictorocosta@gmail.com>
2024-02-05 04:33:28 -0300

    Integes comparison

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::io; 
+use std::cmp::Ordering; // Ordering is an enum
 use rand::Rng; // Rng is a trait
+
 
 fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101); // gen_range is a method on the Rng trait
-    println!("The secret is: {}", secret_number);
+    // println!("The secret is: {}", secret_number);
 
     println!("Guess the number!");
 
@@ -16,7 +18,13 @@ fn main() {
         .read_line(&mut guess) // read_line returns a Result type
         .expect("Failed to read line"); // expect is a method on Result types
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // subscribing guess to a new variable with a integer type
     
-
+    //compare guess to secret_number
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
 }
