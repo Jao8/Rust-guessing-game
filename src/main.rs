13726e0bbb861a85ec54fe095fb5821e5e3cf9bf
Jao <joaovictorocosta@gmail.com>
2024-02-05 04:27:22 -0300

    Got random number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::io; 
+use rand::Rng; // Rng is a trait
 
 fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1..101); // gen_range is a method on the Rng trait
+    println!("The secret is: {}", secret_number);
+
     println!("Guess the number!");
 
     println!("Please input your guess: ");
@@ -11,5 +16,7 @@ fn main() {
         .read_line(&mut guess) // read_line returns a Result type
         .expect("Failed to read line"); // expect is a method on Result types
 
-    println!("You guessed: {}", guess);   
+    
+
+
 }
